1b0b6bd3c0987ad687de50a9078fb8e148eb87a5
WangPanFJG <375277023@qq.com>
2018-06-13 22:42:15 +0800

    views

--- server/index.js
+++ server/index.js
@@ -1,19 +1,33 @@
 const Koa = require('koa')
-const ejs = require('ejs')
-const pug = require('pug')
-const app = new Koa()
-const { html, ejsTpl, pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
+const views = require('koa-views')
 
+const { resolve } = require('path')
+
+const app = new Koa()
+// const { html, ejsTpl, pugTpl } = require('./tpl')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug' // 指定模板为pug
+}))
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8'
-  // ctx.body = ejs.render(ejsTpl, {
-  //   you: 'look',
-  //   me: 'main'
-  // })
-  ctx.body = pug.render(pugTpl, {
-    you: 'pug',
-    me: 'main'
-  })
+    await ctx.render('index', { // index.pug
+      you: 'views-pug',
+      me: 'main'
+    })
+    // 由于引入了views, 所以此时render方法已经被views挂载到了ctx上
 })
+// app.use(async (ctx, next) => {
+//   ctx.type = 'text/html; charset=utf-8'
+//   // ctx.body = ejs.render(ejsTpl, {
+//   //   you: 'look',
+//   //   me: 'main'
+//   // })
+//   ctx.body = pug.render(pugTpl, {
+//     you: 'pug',
+//     me: 'main'
+//   })
+// })
 
 app.listen(7777)
