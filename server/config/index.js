7cdef36b77afc224b9f730410713b1aa1f090191
main <375277023@qq.com>
2018-06-15 14:38:02 +0800

    上传封面图和视频到七牛空间

---
+++ server/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "qiniu": {
+    "bucket": "node-demo", // 七牛的存储空间名
+    // "video": "http://video.iblack7.com/", // 配置访问 video 的域名
+    "video": "http://pacopvi05.bkt.clouddn.com", // 配置访问 video 的域名
+    // "AK": "-dLG8KAfn4_fmsxZBfeqDgrtyM7e3rIzQezhdxvk",
+    "AK": "ShzfAMioVNgnEhpVzHN_N3qmtXYD_2f59bSBPr8R",
+    // "SK": "wcgqiwkL7QaX5Bz8OtyJEfLuAFb_qCrqFg_eNbW0"
+    "SK": "tuWBJCYuKreU8uDhxabOZXC_1eTvRTu3HsmrX1A8"
+  }
+}
